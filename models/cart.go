ccf817aded957012933c99c6794d372ee75eca8b
20pa5a1210 <20pa5a1210@vishnu.edu.in>
2023-06-23 22:04:20 +0530

    feat: cart product  Model (#1)

---
+++ models/cart.go
@@ -0,0 +1,18 @@
+package models
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Cart struct {
+	Id          primitive.ObjectID `json:"_id" bson:"_id,omitempty"`
+	ID          string             `json:"id" bson:"id"`
+	Name        string             `json:"name" bson:"name"`
+	Brand       string             `json:"brand" bson:"brand"`
+	Price       float64            `json:"price" bson:"price"`
+	Rating      float64            `json:"rating" bson:"rating"`
+	Type        string             `json:"type" bson:"type"`
+	Image       string             `json:"image" bson:"image"`
+	Description string             `json:"description" bson:"description"`
+	Quantity    int64              `json:"quantity" bson:"quantity"`
+}
